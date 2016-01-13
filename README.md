53f0c58bb397d57fc9d74e69cda09f932736465e
jacethelefty <jacemontoya@live.com>
2016-01-13 12:42:28 -0800

    initial commit with new files added

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 #Basic HTTP Server
 
+##Jason Montoya
+
 ##To Submit this Assignment
   * fork this repository
   * write all of your code in a folder containing your name
@@ -16,7 +18,7 @@ The server should respond to a request to /time that will send back the current 
 It should also respond to a get request to /greet/name where name is any single word string. It should send back a string that greets that name.
 
 It should also have a separate post request to /greet that takes the name in JSON format.
- 
+
 
 There should be tests using chaiHTTP for both routes, as well as a Gruntfile/package.json
 
@@ -24,3 +26,13 @@ There should be tests using chaiHTTP for both routes, as well as a Gruntfile/pac
   * Tests: 4pts
   * Routes: 4pts
   * Organization and gulpfile/package.json 2pts
+
+##References:
+
+https://github.com/jacethelefty/create_an_http_server/blob/master/jack_sneed/server.js
+
+https://github.com/jacethelefty/create_an_http_server/blob/master/walter_nicholas/server.js
+
+https://github.com/jacethelefty/create_an_http_server/blob/master/john_tucker_miller/test/test.js
+
+
---
+++ server.js
@@ -0,0 +1,53 @@
+const http = require('http'),
+      fs = require('fs');
+var date = new Date(),
+    currentTime = date.toString(),
+    PORT = 3000;
+
+var server = http.createServer(function(request, response) {
+  var responseData = {},
+      urlPath =request.url.split('/'),
+      url = request.url,
+      rD = responseData;
+
+
+  function writeResponse() {
+    response.writeHead(rD.status || 404, {
+      'Content-Type': rD.contentType || 'text/plain'
+    });
+    response.write(rD.data || 'not found');
+    response.end();
+  }   //end writeResponse function
+
+  if (url === '/' && request.method === 'GET') {
+    rD.status = 200;
+    rD.contentType = 'text/html';
+    rD.data = fs.readFileSync(__dirname + '/public/index.html');
+    writeResponse();
+  } else if (urlPath[1] === 'greet' && request.method === 'GET') {
+      rD.status = 200;
+      rD.contentType = 'text/plain';
+      var name = request.url.substring(7, request.url.length);
+      rD.data = 'Hola my amigo ' + name + '.';
+      writeResponse();
+  } else if (urlPath[1] = 'greet' && request.method === 'POST') {
+      rD.status = 200;
+      rD.contentType = 'text/plain';
+      rD.data = 'post detected';
+
+      request.on('data', function(data) {
+        var obj = JSON.parse(data);
+        rD.data = 'Hola ' + obj.name + '.';
+        writeResponse();
+      });
+  } else if(urlPath[1] = 'time' && request.method === 'GET') {
+      rD.status = 200;
+      rD.contentType = 'text/plain';
+      rD.data = currentTime;
+      writeResponse();
+    }
+});   //end http.createServer function
+
+server.listen(PORT, function() {
+  console.log('server up');
+});

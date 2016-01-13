8ff7d8299fc2ebcd1c3bd923a43f3b300c95da0f
ERIKAWHO <Erika@RefreshMediaResources.com>
2016-01-13 11:56:27 -0800

    Updated package.json and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,42 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
-gulp.task('default', ['mocha_tests', 'watch']);
+gulp.task('default', ['eslint_check', 'mocha_tests', 'watch']);
 gulp.task('function', function() {
   /* Place code for your "function" task here, what is run when:
   $gulp function
   is called. */
+});
+
+gulp.task('eslint_check', function() {
+  return gulp.src(['**/*.js','!node_modules/**','!dist/*.js']).pipe(plugins.eslint(
+    {
+      'settings': {
+        'ecmascript': 5
+      },
+      'ecmaFeatures': {
+      },
+      'env': {
+        'browser': true,
+        'jquery': true,
+        'node': true,
+        'mocha': true
+      },
+      'rules': {
+        'semi': 1,
+        'strict': 0,
+        'indent': [2, 2],
+        'quotes': [1, 'single'],
+        'no-multi-spaces': [1, {
+          'exceptions': {
+            'VariableDeclarator': true,
+            'FunctionExpression': true
+          }
+        }],
+        'key-spacing': [0, {'align': 'value'}],
+        'no-underscore-dangle': 0
+      }
+    }
+  )).pipe(plugins.eslint.format()).pipe(plugins.eslint.failAfterError());
 });
 
 gulp.task('mocha_tests', function () {
@@ -12,5 +44,5 @@ gulp.task('mocha_tests', function () {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['**/*.js', '!package.json', '!node_modules/**'], ['mocha_tests']);
+  gulp.watch(['**/*.js', '!package.json', '!node_modules/**'], ['eslint_check', 'mocha_tests']);
 });

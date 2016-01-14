2eca99d95ba0f13c831c4d2558c1baace3a74eb2
jessethach <jessethach@gmail.com>
2016-01-14 10:50:29 -0800

    finalized http server and added post test script into test file. refactored code to properly test post request

--- jessethach/lib/http-server.js
+++ jessethach/lib/http-server.js
@@ -1,38 +1,48 @@
 const http = require('http');
 const fs = require('fs');
+const greet = require(__dirname + '/greet');
 
 var server = module.exports = exports = http.createServer((req, res) => {
-  var greet = {};
   var timeRequested = {};
-  var name = {};
   if(req.method === 'GET' && req.url === '/') {
     console.log('Request for ' + req.url + ' received');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello world ');
     return res.end();
-  };
+  }
 
-  if(req.method === 'GET' && req.url === '/time') {
+  else if(req.method === 'GET' && req.url === '/time') {
     console.log('Getting time');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     timeRequested = 'Time logged at ' + new Date().toString() + ' ';
     res.write(timeRequested);
-    console.log(timeRequested);
     return res.end();
-  };
+  }
 
-  if(req.method === 'POST' && req.url === '/jesse') {
+  else if(req.method === 'GET' && req.url.startsWith('/greet')) {//req.url startswith credit to Natalie Chow
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    greet = 'Hello, ';
-    name = req.url.split('/')[1].toUpperCase();
-    res.write(greet + name);
-    console.log(greet);
+    var name = req.url.split('/')[2];
+    res.write(greet(name));
     return res.end();
-  };
+  }
 
-  res.writeHead(404, {'Content-Type': 'text/plain'});
-  res.write('Page not found ');
-  return res.end();
+  else if(req.method === 'POST' && req.url === '/greet') {
+    var resBody= '';
+    req.on('data', (chunk) => {
+      resBody += chunk;
+    });
+    req.on('end', () => {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(greet(JSON.parse(resBody).name));
+      res.end();
+    });
+  }
+  
+  else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.write('Page not found ');
+    return res.end();
+  }
 });
 
 server.listen(3000, () => console.log('Server up'));
--- jessethach/test/test-http-server.js
+++ jessethach/test/test-http-server.js
@@ -1,10 +1,11 @@
 const chai = require('chai');
 const expect = require('chai').expect;
-const server = require(__dirname + '/../lib/http-server.js');
 const chaiHttp = require('chai-http');
-const fs = require('fs');
 chai.use(chaiHttp);
+const fs = require('fs');
 const request = chai.request;
+
+const server = require(__dirname + '/../lib/http-server.js');
 
 describe('HTTP server', () => {
   it('should have run server', (done) => {
@@ -18,17 +19,40 @@ describe('HTTP server', () => {
       });
     });
 
-  it('should have a greet route', (done) => {
+  it('should greet tom after curl', (done) => {
     request('localhost:3000')
-      .post('/jesse')
-      .send({name: 'Jesse'})
+      .get('/greet/tom')
       .end((err, res) => {
         expect(err).to.eql(null);
         expect(res).to.have.status(200);
-        expect(res.text).to.eql('Hello, JESSE');
+        expect(res.text).to.eql('Hello, tom ');
         done();
       });
     });
+
+  it('should have a greet route', (done) => {
+    request('localhost:3000')
+      .post('/greet')
+      .send({name: 'jesse'})
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.text).to.eql('Hello, jesse ');
+        done();
+      });
+    });
+
+  // it('should respond to a POST request to /greet', (done) => {
+  //     request('localhost:3000')
+  //       .post('/greet')
+  //       .send({name: 'jesse'})
+  //       .end((err, res) => {
+  //         expect(err).to.eql(null);
+  //         expect(res).to.have.status(200);
+  //         expect(res.text).to.eql('Hello from the other side');
+  //         done();
+  //     });
+  //   });
 
   it('should 404 on a page that does not exist', (done) => {
     request('localhost:3000')

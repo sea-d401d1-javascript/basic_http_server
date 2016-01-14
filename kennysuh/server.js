938517a7f77de93943136f02b2ce76cb0b1fff35
suhk <kennethgenesuh@gmail.com>
2016-01-14 13:16:43 -0800

    Fix content type

--- kennysuh/server.js
+++ kennysuh/server.js
@@ -20,7 +20,7 @@ var server = module.exports = http.createServer(function(req, res) {
     });
 
     req.on('end', function() {
-      res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+      res.writeHead(200, "OK", {'Content-Type': 'application/json'});
       res.end();
     });
 

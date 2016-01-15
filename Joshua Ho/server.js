2cecf4691b47c6f4ea8a40f3c2f2f528b59a1294
ricecreamdude <ho.joshua4@gmail.com>
2016-01-15 10:25:26 -0800

    Second commit - Completed tests and server

--- Joshua Ho/server.js
+++ Joshua Ho/server.js
@@ -13,23 +13,45 @@ var server = module.exports = exports = http.createServer( ( req, res ) => {
 		var newDate = (new Date).toString();
 
 		res.writeHead(200 , {'Content-Type' : 'application/json'});
-		res.write(JSON.stringify({date: newDate	})); // Why aren't these in quotation marks?
-		return res.end();
+		res.write(JSON.stringify({date: newDate	})); 								// Why aren't these in quotation marks?
+		return res.end();	//Closes the connection
+
 	}
 
 	// /greet/name should prompt whatever is after greet/
+	//THIS WORKS BTW
+	if (('/' + urlArray[1]) === '/greet') {
+		if (req.method === 'GET'){
 
-	if (('/' + urlArray[1]) === '/greet'  && req.method === 'GET') {
+			var theName = urlArray[2].toString();
 
-		var dispName = req.url;
+			res.writeHead(200 , {'Content-Type' : 'application/json'});
+			res.write(JSON.stringify({name: theName}));
 
-		res.writeHead(200 , {'Content-Type' : 'application/json'});
-		res.write( JSON.stringify( {name: dispName} ) );
-		return res.end();
+			return res.end();
+		}
+		//To make a POST, use Superagent CLI or something else!
+		//Accepts objects!!!
+		if (req.method === 'POST') {
 
-	}
+			var coolObject = "";
 
-	if (('/' + urlArray[1]) ==='/' || req.url === 'index.html' && req.method === 'GET') {
+			//Accepts a JSON object, parses it into a Javascript object and retrieves .name property. 
+			req.on('data' , (chunk) => {
+			 coolObject += JSON.parse(chunk.toString()).name;
+			});
+
+			req.on('end' , () => {
+				res.writeHead(200 , {'Content-Type' : 'application/json'});
+				res.write(JSON.stringify({name: 'hello ' + coolObject}));		//Responds with a JSON string
+
+				return res.end();
+			});
+		}
+	} 
+
+
+	if ((('/' + urlArray[1]) ==='/' || req.url === 'index.html') && req.method === 'GET') {
 
 		res.writeHead(200 , {'Content-Type' : 'text/html'});
 		var index = fs.createReadStream( __dirname + '/public/index.html');
@@ -48,6 +70,6 @@ server.listen( port , () => {
 });
 
 
-
 // /name takes the request as a post request.
+
 

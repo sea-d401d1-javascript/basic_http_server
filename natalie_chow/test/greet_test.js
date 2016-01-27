338ec7e959d35f498308e4bf217b5a81f9e2884c
Cameron Forras <cameronforras@hotmail.com>
2016-01-27 15:14:15 -0800

    changed folder name to cameron_forras

--- natalie_chow/test/greet_test.js
+++
@@ -1,9 +0,0 @@
-const expect = require('chai').expect;
-
-const greet = require(__dirname + '/../lib/greet');
-
-describe('Greet function', () => {
-  it('should say hello with given name', () => {
-    expect(greet('world')).to.eql('Hello world');
-  });
-});
--- sabrina/index.js
+++
@@ -1,3 +0,0 @@
-const httpServer = require(__dirname + '/lib/http-server');
-
-httpServer.server.listen(3000, () => console.log('Server started!'));
